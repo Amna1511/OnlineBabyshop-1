bdcf679e1df1820c22d6dac89263bc2fc412dc41
Amna1511 <59605940+Amna1511@users.noreply.github.com>
2020-02-07 10:45:03 +0100

    Update IOrdersRepository.cs

--- OnlineBabyshop/Data/Interfaces/IOrdersRepository.cs
+++ OnlineBabyshop/Data/Interfaces/IOrdersRepository.cs
@@ -4,25 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 89dfc6c724e9a8493a70b7d55b5cf46b63d3eb62
-namespace OnlineBabyshop.Data.Interfaces
-{
-    public interface IOrdersRepository
-    {
-        void CreateOrder(Order order);
-        void CreateOrderDetails(OrderDetails orderDetails);
-<<<<<<< HEAD
-=======
-=======
 namespace OnlineBabyshop.Data.Repositories
 {
    public interface IOrdersRepository
     {
         int CreateOrder(Orders orders);
->>>>>>> f59acac8d55b8772f369c689cbacb105318e8a3a
->>>>>>> 89dfc6c724e9a8493a70b7d55b5cf46b63d3eb62
+
     }
 }
